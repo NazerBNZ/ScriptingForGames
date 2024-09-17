01e2fd44bb9d540e6cc917695a3f9dc528667c12
Mercedes Nazer <11031090@uvu.edu>
2024-09-16 20:08:13 -0600

    Added file for scripting 3 lab

---
+++ scripting3_lab/Assets/Scripts/ApplyForce.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyForce : MonoBehaviour
+{
+    private Rigidbody rb;
+   
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.right * 600);
+    }
+}
