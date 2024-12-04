c43fa660813b6c9c46edf3974db7c33bae886be2
Mercedes Nazer <11031090@uvu.edu>
2024-12-04 15:28:05 -0700

    lab 12 wip

---
+++ scripting4/Assets/Scripts/SimpleNavMeshAgent.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleNavMeshAgent : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshAgent agent;
+    
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+    }
+    
+    private void Update()
+    {
+        if (target != null)
+        {
+            NavMeshAgent.SetDestination(target.position);
+        }
+    }
+}
